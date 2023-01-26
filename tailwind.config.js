7db3af7898f2744b66b5fa501edaa58b0f25eb32
NIKHIL MATTA <67046318+nikhil478@users.noreply.github.com>
2023-01-26 17:16:57 +0530

    css error fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  enabled: process.env.NODE_ENV === "production",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",

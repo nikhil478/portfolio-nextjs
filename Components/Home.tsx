0a7717febd04d3f118f2114987fe4b1e083c0a30
NIKHIL MATTA <67046318+nikhil478@users.noreply.github.com>
2023-01-26 20:05:32 +0530

    Home addition

---
+++ Components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="bg-gray-100 flex flex-col items-center justify-center h-screen">
+        <h1 className="text-5xl font-bold text-gray-800">Welcome to my Portfolio</h1>
+    <p className="text-xl text-gray-600">Take a look around to learn more about my work and experience.</p>
+    </div>
+)
+}
+
+export default Home;

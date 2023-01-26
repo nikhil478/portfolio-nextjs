30bf8dfc85abd337b73b596fd0677188de9906c9
NIKHIL MATTA <67046318+nikhil478@users.noreply.github.com>
2023-01-26 17:06:04 +0530

    css error fixing

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div className="fixed w-full h-3% shadow-xl z-[100]">
             <div className="flex justify-between items-center w-full h-2% px-3 2xl:px-16">
-                <Image src="/assets/images.png" alt="/" width="125" height="10"/>
+                {/*<Image src="/assets/images.png" alt="/" width="125" height="10"/>*/}
                 <div>
                     <ul className="hidden md:flex">
                         <Link href="/">
@@ -27,16 +27,16 @@ const Navbar = () => {
                         </Link>
                     </ul>
                     <div className="hidden md:hidden">
-                        <AiOutlineMenu size={25}/>
+                        {/*<AiOutlineMenu size={25}/>*/}
                     </div>
                 </div>
             </div>
             <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
                 <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
                     <div className="flex w-full items-center justify-between">
-                        <Image src="/assets/images.png" alt="/" width="87" height="35"/>
+                        {/*<Image src="/assets/images.png" alt="/" width="87" height="35"/>*/}
                         <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
-                        <AiOutlineClose />
+                        {/*<AiOutlineClose />*/}
                         </div>
                     </div>
                     <div className="border-b border-gray-300 my-4">
